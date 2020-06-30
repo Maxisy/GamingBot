bd28e6ee4b9367c8eed83dc5733c366b4fef723b
Maxisy <maks.sybicki@gmail.com>
2020-06-30 22:08:14 +0200

    added new command

--- src/main/java/me/maxisy/gamingbot/Main.java
+++ src/main/java/me/maxisy/gamingbot/Main.java
@@ -2,6 +2,8 @@ package me.maxisy.gamingbot;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.server.Server;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,6 +25,31 @@ public class Main {
                 long id = event.getMessage().getAuthor().getId();
                 event.getChannel().sendMessage("<@" + id + "> potrzebuje tutaj <@&725658664689598504>!");
             }
+            if (event.getMessage().getContent().contains("!pochwal")) {
+                String content = event.getMessage().getContent();
+                if (event.getMessage().getAuthor().isServerAdmin()) {
+                    if (content.contains(" ")) {
+                        String[] split = content.split(" ");
+                        sendPochwalonoMessage(split, api);
+                        event.getMessage().getChannel().sendMessage("Sukces, wejdz na <#727587187306266755>");
+                    }
+                } else {
+                    event.getMessage().getChannel().sendMessage("Nie masz uprawnien!");
+                }
+            }
+            if (event.getMessage().getContent().contains("kto gra")) {
+                if (!event.getMessage().getChannel().getIdAsString().equals("718571030154707074")) {
+                    event.getMessage().delete();
+                    event.getMessage().getChannel().sendMessage("Zly kanal, wejdz na <#718571030154707074>");
+                }
+            }
+        });
+
+    }
+
+    public static void sendPochwalonoMessage(String[] split, DiscordApi api) {
+        api.getTextChannelById("727587187306266755").ifPresent(textChannel -> {
+            textChannel.sendMessage(" > Pochwalono " + split[1]);
         });
     }
 }
