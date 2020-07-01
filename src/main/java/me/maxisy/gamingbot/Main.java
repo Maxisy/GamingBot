edb545d74defb658359d6bc0f6ddc748cbd73f0d
Maxisy <maks.sybicki@gmail.com>
2020-07-01 15:08:02 +0200

    zmieniono komendę !pochwal i dodano 2 nowe: !py oraz !cp

--- src/main/java/me/maxisy/gamingbot/Main.java
+++ src/main/java/me/maxisy/gamingbot/Main.java
@@ -2,12 +2,21 @@ package me.maxisy.gamingbot;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
-import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.server.Server;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Main {
+    public static final String fileName = "pochwaly.txt";
+
+    static Map<String, Integer> pochwaly = new HashMap<>();
+
     public static void main(String[] args) {
         String token = Private.TOKEN;
+        File f = new File(fileName);
+
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
@@ -29,9 +38,16 @@ public class Main {
                 String content = event.getMessage().getContent();
                 if (event.getMessage().getAuthor().isServerAdmin()) {
                     if (content.contains(" ")) {
-                        String[] split = content.split(" ");
-                        sendPochwalonoMessage(split, api);
-                        event.getMessage().getChannel().sendMessage("Sukces, wejdz na <#727587187306266755>");
+                        try {
+                            String[] split = content.split(" ");
+                            if (!f.exists()) {
+                                f.createNewFile();
+                            }
+                            pochwal(split[1]);
+                            event.getMessage().getChannel().sendMessage("Sukces");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 } else {
                     event.getMessage().getChannel().sendMessage("Nie masz uprawnien!");
@@ -43,13 +59,65 @@ public class Main {
                     event.getMessage().getChannel().sendMessage("Zly kanal, wejdz na <#718571030154707074>");
                 }
             }
+            if (event.getMessage().getContent().contains("!py")) {
+                if (!f.exists()) {
+                    event.getMessage().getChannel().sendMessage("> Nie ma zadnych pochwal!");
+                } else {
+                    event.getMessage().getChannel().sendMessage("> Pochwaly:");
+                    for (Map.Entry<String, Integer> entry : pochwaly.entrySet()) {
+                        String key = entry.getKey();
+                        int value = entry.getValue();
+                        event.getMessage().getChannel().sendMessage("> " + key + ": " + value);
+                    }
+                }
+            }
+            if (event.getMessage().getContent().contains("!cp")) {
+                if (event.getMessage().getAuthor().getIdAsString().equals("434346676937818122")) {
+                    if (event.getMessage().getContent().contains(" ")) {
+                        String[] splitted = event.getMessage().getContent().split(" ");
+                        pochwaly.remove(splitted[1]);
+                        event.getMessage().getChannel().sendMessage("> Sukces.");
+                    }
+                } else {
+                    event.getMessage().getChannel().sendMessage("Nie masz uprawnien!");
+                }
+            }
         });
 
     }
 
-    public static void sendPochwalonoMessage(String[] split, DiscordApi api) {
-        api.getTextChannelById("727587187306266755").ifPresent(textChannel -> {
-            textChannel.sendMessage(" > Pochwalono " + split[1]);
-        });
+    public static void pochwal(String u) {
+        try (
+                FileWriter fw = new FileWriter(fileName, true);
+                BufferedWriter writer = new BufferedWriter(fw);
+        ) {
+            writer.write(u);
+            writer.newLine();
+            pochwaly.put(u, 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (
+                FileReader fr = new FileReader(fileName);
+                BufferedReader reader = new BufferedReader(fr)
+        ) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.equals(u)) {
+                    int ps = pochwaly.get(u);
+                    pochwaly.put(u, ++ps);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+//    public static void sendPochwalonoMessage(String[] split, DiscordApi api) {
+//        api.getTextChannelById("727587187306266755").ifPresent(textChannel -> {
+//            textChannel.sendMessage(" > Pochwalono " + split[1]);
+//        });
+//    }
 }
